34eeaca6d48518b13d01e14e128fab24b44fb806
Sigmund Frost <sigmafi.tech@gmail.com>
2024-06-15 23:52:57 +0200

    Minor fixes

--- next/ui-components/app/components/reUse-button.tsx
+++ next/ui-components/app/components/reUse-button.tsx
@@ -4,8 +4,7 @@ import { MdCancel } from "react-icons/md"
 import { FaRegSave } from "react-icons/fa"
 
 type RUButtonProps = {
-    customStyle?: string
-    customFunction?: () => void
+    customStyle?: string 
     title: string
     icon?: JSX.Element
     disabled?: boolean
@@ -13,7 +12,6 @@ type RUButtonProps = {
 
 type RUPresetProps = {
     customStyle?: string
-    customFunction?: () => void
     disabled?: boolean
 }
 
@@ -26,87 +24,78 @@ const changeCase = (str: string) => {
     }
 }
 
-function ReUseButton({ customStyle, customFunction, title, icon, disabled }: RUButtonProps) {
+function ReUseButton({ customStyle, title, icon, disabled }: RUButtonProps) {
     const filteredTitle = changeCase(title)
     return (
-        <div>
-            <button
-                role={`${title} button`}
-                aria-label={title}
-                className={`flex items-center justify-center
-                            min-w-[96px] min-h-[48px] px-4 py-2
-                            text-white text-lg rounded-md 
-                            mx-[8px] cursor-pointer
-                            bg-blue-600 hover:bg-blue-700
-                            focus:outline-none target:bg-blue-700
-                            disabled:bg-gray-700 disabled:cursor-not-allowed
-                            drop-shadow-md
-                            select-none
-                            transition-all ease-in-out duration-200
-                            ${customStyle || ''}
-                            `}
-                onClick={customFunction}
-                disabled={disabled}
-            >
-                {filteredTitle}<span className='ml-2'>{icon}</span>
-            </button>
-        </div>
+        <button
+            role={`${title} button`}
+            aria-label={title}
+            className={`flex items-center justify-center
+                        min-w-[96px] min-h-[48px] px-4 py-2
+                        text-white text-lg rounded-md 
+                        mx-[8px] cursor-pointer
+                        bg-blue-600 hover:bg-blue-700 active:bg-blue-800
+                        focus:outline-none target:bg-blue-700
+                        disabled:bg-gray-700 disabled:cursor-not-allowed
+                        drop-shadow-md
+                        select-none
+                        transition-all ease-in-out duration-200
+                        ${customStyle || ''}
+                        `}
+            disabled={disabled}
+        >
+            {filteredTitle}<span className='ml-2'>{icon}</span>
+        </button>
     )
 }
 
 export default ReUseButton
 
-function ReUseCancel({ customStyle, customFunction, disabled }: RUPresetProps) {
+function ReUseCancel({ customStyle, disabled }: RUPresetProps) {
     return (
-        <div>
-            <button
-                role={`cancel button`}
-                aria-label={`cancel`}
-                className={`flex items-center justify-center
-                            min-w-[96px] min-h-[48px] px-4 py-2
-                            text-white text-lg rounded-md 
-                            mx-[8px] cursor-pointer
-                            bg-red-600 hover:bg-red-700
-                            focus:outline-none target:bg-red-700
-                            disabled:bg-gray-700 disabled:cursor-not-allowed
-                            drop-shadow-md
-                            select-none
-                            transition-all ease-in-out duration-200
-                            ${customStyle || ''}
-                            `}
-                onClick={customFunction}
-                disabled={disabled}
-            >
-                CANCEL<span className='ml-1 text-2xl'><MdCancel /></span>
-            </button>
-        </div>
+        <button
+            role={`cancel button`}
+            aria-label={`cancel`}
+            className={`flex items-center justify-center
+                        min-w-[96px] min-h-[48px] px-4 py-2
+                        text-white text-lg rounded-md 
+                        mx-[8px] cursor-pointer
+                        bg-red-600 hover:bg-red-700 active:bg-red-800
+                        focus:outline-none target:bg-red-700
+                        disabled:bg-gray-700 disabled:cursor-not-allowed
+                        drop-shadow-md
+                        select-none
+                        transition-all ease-in-out duration-200
+                        ${customStyle || ''}
+                        `}
+            disabled={disabled}
+        >
+            CANCEL<span className='ml-1 text-2xl'><MdCancel /></span>
+        </button>
     )
 }
 
-function ReUseSave({ customStyle, customFunction, disabled }: RUPresetProps) {
+function ReUseSave({ customStyle, disabled }: RUPresetProps) {
     return (
-        <div>
-            <button
-                role={`save button`}
-                aria-label={`save`}
-                className={`flex items-center justify-center
-                            min-w-[96px] min-h-[48px] px-4 py-2
-                            text-white text-lg rounded-md 
-                            mx-[8px] cursor-pointer
-                            bg-green-600 hover:bg-green-700
-                            focus:outline-none target:bg-green-700
-                            disabled:bg-gray-700 disabled:cursor-not-allowed
-                            drop-shadow-md
-                            select-none
-                            transition-all ease-in-out duration-200
-                            ${customStyle || ''}
-                            `}
-                onClick={customFunction}
-                disabled={disabled}
-            >
-                SAVE<span className='ml-2'><FaRegSave /></span>
-            </button>
-        </div>
+        <button
+            role={`save button`}
+            aria-label={`save`}
+            className={`flex items-center justify-center
+                        min-w-[96px] min-h-[48px] px-4 py-2
+                        text-white text-lg rounded-md 
+                        mx-[8px] cursor-pointer
+                        bg-green-600 hover:bg-green-700 active:bg-green-800
+                        focus:outline-none target:bg-green-700
+                        disabled:bg-gray-700 disabled:cursor-not-allowed
+                        drop-shadow-md
+                        select-none
+                        transition-all ease-in-out duration-200
+                        ${customStyle || ''}
+                        `}
+            disabled={disabled}
+        >
+            SAVE<span className='ml-2'><FaRegSave /></span>
+        </button>
     )
 }
 
--- next/ui-components/app/page.tsx
+++ next/ui-components/app/page.tsx
@@ -1,10 +1,11 @@
 import ReUseButton, { ReUseCancel, ReUseSave } from "./components/reUse-button"
 import { FaCheck } from "react-icons/fa"
 
+
 export default function Home() {
   return (
     <div className="flex justify-center mt-7">
-      <ReUseButton title='Action' icon={<FaCheck />} />
+      <ReUseButton title='Action' icon={<FaCheck />} />      
       <ReUseCancel />
       <ReUseSave />
     </div>
